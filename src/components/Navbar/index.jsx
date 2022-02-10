d3024d37a569bed41d9e4c2713c19059a4f44166
Niyazova_Gulnoza <niyazovagulnoza2@gmail.com>
2022-02-10 11:32:10 +0200

    nav-html is completed

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -3,12 +3,21 @@ import Container from './style'
 
 function Navbar() {
   return <Container>
-      <div className="header">
-          <li>
-              <a href=""></a>
-          </li>
+      <div className="navbar">
+          <p className="nav__logo">Genius</p>
+          <ul className='nav__list'>
+                <li className="nav__item">Home</li>
+                <li className="nav__item">Courses</li>
+                <li className="nav__item"> Webinar</li>
+                <li className="nav__item">Pricing</li>
+                <li className="nav__item">
+                    
+                </li>
+                <li className="nav__item">
+                    <button className='nav__btn'>Login</button>
+                </li>
+          </ul>
       </div>
-      navbar
   </Container>;
 }
 
