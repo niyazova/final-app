7300a78277d842fedaaad98e6997ca5dcc615a05
Niyazova_Gulnoza <niyazovagulnoza2@gmail.com>
2022-02-11 17:16:10 +0200

    navbar structured

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Container from './style'
+import Container from './style.jsx'
 
 function Navbar() {
   return <Container>
@@ -10,8 +10,12 @@ function Navbar() {
                 <li className="nav__item">Courses</li>
                 <li className="nav__item"> Webinar</li>
                 <li className="nav__item">Pricing</li>
-                <li className="nav__item">
-                    
+                <li className="nav__item"> 
+                    <select id="languages">
+                        <option value="Eng">Eng</option>
+                        <option value="Rus">Rus</option>
+                        <option value="Uzb">Uzb </option>
+                    </select>
                 </li>
                 <li className="nav__item">
                     <button className='nav__btn'>Login</button>
--- src/components/Navbar/style.js
+++ src/components/Navbar/style.js
@@ -2,15 +2,19 @@ import sty from 'styled-components'
 
 
 const Container = sty.div`
-    background-color: blue;
-
-
-    .header{
-
-        li{
-            
+    .navbar{
+        width: 100vh;
+        height:60px
+        margin: 40px;
+        padding: 0; 
+        position: fixed;
+        display: flex;
+        justify-content: space-between;
+        align-item: center;
+        color: 
+        .nav__list {
+            display:flex;
         }
-
     }
 
 `
