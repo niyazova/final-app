7db3e6a8c7670acfc3625b40efdf441ac4b7fcbf
Niyazova_Gulnoza <niyazovagulnoza2@gmail.com>
2022-02-13 00:33:04 +0200

    users is added

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,12 +1,29 @@
 import React from 'react'
 import Header from './styled.js'
 import home_img from '../../assets/home-bg.png'
+import user1 from '../../assets/user.png'
+import user2 from '../../assets/user2.png'
+import user3 from '../../assets/user3.png'
+import user4 from '../../assets/user4.png'
 
 function Index() {
   return <Header>
       <div className="header">
             <div className="header__left">
                 <img className='left__img' src={home_img} alt="#" />
+
+                <div className="left__user">
+                  <div className="users__imgs">
+                      <img className='user__img'  src={user1}  alt="#" />
+                      <img className='user__img2'  src={user2}  alt="#" />
+                      <img className='user__img3'  src={user3}  alt="#" />
+                      <img className='user__img4'  src={user4}  alt="#" />
+                      <span className='user__img5'>+</span>
+                  </div>
+                  <div className="users__title">
+                      <p>More than 80k+ Students joined!</p>
+                  </div>
+                </div>
             </div>
 
             <div className="header__right">
--- src/components/Header/styled.js
+++ src/components/Header/styled.js
@@ -7,9 +7,9 @@ const Container = sty.div`
     display: flex;
 
     .header__left {
-        width: 580px;
-        height: 580px;
-        margin-top: 180px;
+        width: 620px;
+        height: 620px;
+        margin-top: 130px;
         background: bottom no-repeat;
         background-image: url(${back_img});
         position: relative;
@@ -17,11 +17,63 @@ const Container = sty.div`
 
         .left__img {
             position: absolute;
-            margin-top: -81px;
-            margin-left: 100px;
+            margin-top: -77px;
+            margin-left: 80px;
             height: 100%;
-            transform: rotate(-8deg);
+            transform: rotate(-7deg);
             width: 100%;
+        }
+        .left__user {
+            position:absolute;
+            height:80px;
+            width: 310px;
+            border: none;
+            margin-left: 380px;
+            margin-top: 455px;
+            border-radius: 16px;
+            display: flex;
+            background: linear-gradient(to bottom, rgba(63, 69, 141, 0.63), rgba(63, 69, 141, 0.14));
+
+            .users__imgs {
+                display: flex;
+                margin-top:15px;
+
+                .user__img{
+                    height:45px;
+                    margin-left: 25px;
+                }
+                .user__img2{
+                    height:45px;
+                    margin-left: -17px;
+                }
+                .user__img3{
+                    height:45px;
+                    margin-left: -17px;
+                }
+                .user__img4{
+                    height:45px;
+                    margin-left: -17px;
+                }
+                .user__img5{
+                    display: flex;
+                    justify-content: center;
+                    align-items: center;
+                    font-size: 26px;
+                    color: #fff;
+                    height:45px;
+                    width:45px;
+                    border-radius:50%;
+                    margin-left: -17px;
+                    background:rgba(63, 69, 141, 0.63);
+                }
+            }
+            .users__title p {
+                font-size: 14px;
+                margin-left:8px;
+                margin-bottom: 25px;
+                color: #fff;
+            }
+
         }
     }
 }
