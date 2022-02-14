6ecf9b51dd436901bdebfe696801ee5f34d4742b
Niyazova_Gulnoza <niyazovagulnoza2@gmail.com>
2022-02-14 22:01:09 +0200

    main is created

---
+++ src/components/Main/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Container from './style'
+
+function Main() {
+  return <Container>
+      
+      
+  </Container>;
+}
+
+
+
+
+export default Main;
---
+++ src/components/Main/style.js
@@ -0,0 +1,9 @@
+import sty from 'style-components'
+
+
+const Container = sty.div`
+    
+
+`
+
+export default Container
