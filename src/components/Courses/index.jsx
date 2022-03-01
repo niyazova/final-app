cdafa0b486f4a8fe7ee35d91f278febae48bb40b
Niyazova_Gulnoza <niyazovagulnoza2@gmail.com>
2022-03-01 10:29:47 +0200

    section__left is completed

--- src/components/Courses/index.jsx
+++ src/components/Courses/index.jsx
@@ -3,9 +3,22 @@ import Container from './styleCourse'
 
 function Courses() {
   return (
-    <div className="section">
-        
-    </div>
+    <Container className="section">
+      <p className='section__logo'>Best</p>
+        <div className='section__left'>
+          <ul className="left__list">
+            <div className=" list__item--logo">Courses</div>
+            <div className="list__item">Design</div>
+            <div className="list__item">Coding</div>
+            <div className="list__item">Technology</div>
+          </ul>
+          <button className="left__button">Add Course</button>
+        </div>
+        <div className='section__right'>
+          
+        <button className="right__button"> Join Course</button>
+        </div>
+    </Container>
   )
 }
 
