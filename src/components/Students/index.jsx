bbcf890a2bdbd44900237c3c43e69d59684c758f
Niyazova_Gulnoza <niyazovagulnoza2@gmail.com>
2022-03-03 09:13:55 +0200

    student page is adding

--- src/components/Students/index.jsx
+++ src/components/Students/index.jsx
@@ -3,9 +3,21 @@ import Container from './styleStudents'
 
 function Students() {
   return (
-    <div className="section">
-        hello
-    </div>
+    <Container className="section">
+         <p className='section__logo'>Best</p>
+        <div className='section__left'>
+          <ul className="left__list">
+            <div className=" list__item--logo">Students</div>
+            <div className="list__item">Design group</div>
+            <div className="list__item">Coding group</div>
+            <div className="list__item">Technology group</div>
+          </ul>
+          <button className="left__button">Add Course</button>
+        </div>
+        <div className='section__right'>
+          
+        </div>
+    </Container>
   )
 }
 
